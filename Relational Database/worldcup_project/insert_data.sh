e4587220a4874393c32e591f92656d8ea5d9dd8e
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-06-27 17:22:47 -0300

    Add files via upload

---
+++ Relational Database/worldcup_project/insert_data.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+COUNTRIES=()
+
+while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+
+
+if [[ ! " ${COUNTRIES[*]} " =~ " $WINNER " && "$WINNER" != 'winner' ]]; then
+COUNTRIES+=("$WINNER")
+$PSQL "INSERT INTO teams (name) VALUES ('$WINNER');"
+fi
+
+if [[ ! " ${COUNTRIES[*]} " =~ " $OPPONENT " && "$OPPONENT" != 'opponent' ]]; then
+COUNTRIES+=("$OPPONENT")
+$PSQL "INSERT INTO teams (name) VALUES ('$OPPONENT');"
+fi
+
+
+WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER';")
+OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT';")
+
+$PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ('$YEAR','$ROUND','$WINNER_ID','$OPPONENT_ID','$WINNER_GOALS','$OPPONENT_GOALS');"
+
+done < games.csv
